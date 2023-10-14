10fb5a4fe67296f03c05c4f470030d28e4f519a4
Christhian Gutierrez <cjgutierrez44@ucatolica.edu.co>
2023-10-14 17:13:29 -0500

    todo valio verdura y me tire el proyecto

--- app_uninstaller.py
+++ app_uninstaller.py
@@ -1,7 +1,24 @@
+import json
+import winrm
 from flask import Blueprint, render_template
+from utils import get_pc_ip
 
 app_uninstaller_bp = Blueprint('app_uninstaller', __name__)
 
 @app_uninstaller_bp.route('/')
+
+def app_unistall(host,app_id):
+    if host==get_pc_ip():
+        session = winrm.Session(f'http://{host}:5985/wsman',auth=('support', 'support12345'))
+    else:
+        session = winrm.Session(f'http://{host}:5985/wsman',auth=('support', 'support12345'),transport='ntlm')
+    powershell_script =f"""
+                        Get-WmiObject -Class Win32_Product | Where-Object {{ $_.IdentifyingNumber -eq "{app_id}" }} | ForEach-Object {{ $_.Uninstall() }}
+                    """
+    session.run_ps(powershell_script)               
+    
 def index():
     return render_template('script_pages/app_uninstaller.html')    
+
+if __name__=='__main__':
+    app_unistall('192.168.20.8',)
--- updates_installer.py
+++ updates_installer.py
@@ -1,10 +1,37 @@
 from flask import Blueprint, render_template, make_response, jsonify
-from utils import process_request
+from utils import get_local_path, get_network, get_pc_ip, process_request
 import time
 import random
+import json
+import winrm
+import os
+
 
 updates_installer_bp = Blueprint('updates_installer', __name__)
 
+
+def update(host):
+    if host==get_pc_ip():
+        session = winrm.Session(f'http://{host}:5985/wsman',auth=('support', 'support12345'))
+    else:
+        session = winrm.Session(f'http://{host}:5985/wsman',auth=('support', 'support12345'),transport='ntlm')
+    x=0
+    
+    powershell_script = f'''
+                        Install-WindowsUpdate -AcceptAll -AutoReboot
+                        '''
+    result = session.run_ps(powershell_script)  
+
+def update_windows():
+    file_database=get_network()
+    path_file=get_local_path() + '/database/'+ file_database.replace('.','_')
+    if os.path.isfile(path_file + '_updates.json'):
+        with open(path_file + '_updates.json', "r") as file:
+            list_hosts = json.load(file)
+        for ir in list_hosts:
+            host=ir['ip']
+            if ir['updates']:
+                update(host)
 
 def install_updates_on_all_hosts():
     time.sleep(5)
@@ -40,3 +67,5 @@ def install_updates_host(ip):
         return process_request(function_result = False)
 
     return process_request(function_result = True, response_msg = "Equipo actualizado.")
+
+
