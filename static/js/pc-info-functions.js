0cafaa044ad6ec79c3b5c9304a7fd07f020685e2
Christhian Gutierrez <cjgutierrez44@ucatolica.edu.co>
2023-10-16 03:16:01 -0500

    Add reload app inventory, refactor datatable, convert external resources to local

--- static/js/pc-info-functions.js
+++ static/js/pc-info-functions.js
@@ -3,6 +3,7 @@ import {setHostUpdate, setAppUninstall, functions, execBackFunction} from './fun
 const UPDATE_PC_BTN = document.getElementById('update_pc_btn');
 const LOAD_UPDATES_BTN = document.getElementById('load_updates_btn');
 const UNINSTALL_APPS_BTNS = document.getElementsByClassName('uninstall-app-btn');
+const LOAD_APPS_BTN = document.getElementById('load_apps_btn');
 
 UPDATE_PC_BTN.addEventListener('click', async function (e) {
     setHostUpdate(e.target.classList[0]);
@@ -11,6 +12,11 @@ UPDATE_PC_BTN.addEventListener('click', async function (e) {
 
 LOAD_UPDATES_BTN.addEventListener('click', async function (e) {
     await execBackFunction(e, functions.LOAD_UPDATES, '');
+    window.location.reload();
+});
+
+LOAD_APPS_BTN.addEventListener('click', async function (e) {
+    await execBackFunction(e, functions.LOAD_APPS, '');
     window.location.reload();
 });
 
